02c041bc63f136005623d443fe5b57d79517ba30
Eric Tiedeken <eric.tiedeken@gmail.com>
2018-01-16 14:41:54 -0600

    Main table calculated

--- src/app/amortization-calculator/amortization-calculator.component.ts
+++ src/app/amortization-calculator/amortization-calculator.component.ts
@@ -9,42 +9,110 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 export class AmortizationCalculatorComponent implements OnInit {
 
     loanAmount: number;
-    length: number;
+    time: number;
     interestRate: number;
 
+    years: number;
+    months: number;
+
+    monthlyPayment: number;
+
     extraPayments: number;
-    extraPaymentsType: boolean;
+    extraPaymentsType: number;
 
-    monthlyPayment;
-
-    table;
+    table: Amortization[];
 
     constructor() { }
 
-    ngOnInit() {}
-
-    loanAmountUpdate(loanAmount) {
-        this.loanAmount = this.elValToNum(loanAmount);
+    ngOnInit() {
+        this.loanAmount = 0;
+        this.time = 0;
+        this.interestRate = 0;
+        this.years = 0;
+        this.months = 0;
+        this.monthlyPayment = 0;
+        this.extraPayments = 0;
+        this.extraPaymentsType = 0;
     }
 
-    lengthUpdate(year, month) {
-        const years = year.value = this.elValToNum(year);
-        const monthTemp = this.elValToNum(month);
-        const months = month.value = monthTemp > 12 ? 12 : monthTemp;
-        this.length = (years * 12) + months;
+    // converts an element's value to a number 0 or greater
+    elValToNum(val): number {
+        const convert: number = parseInt(val, 10);
+        return convert ? convert < 0 ? 0 : convert : 0;
     }
 
-    elValToNum(el) {
-        const convert = parseInt(el.value, 10);
-        return el.value = convert ? convert < 0 ? 0 : convert : 0;
+    loanAmountUpdate(): void {
+        this.loanAmount = this.elValToNum(this.loanAmount);
     }
 
-    interestRateUpdate(interestPercentage) {
-        this.interestRate = interestPercentage / 100;
+    lengthUpdate(): void {
+        this.years = this.elValToNum(this.years);
+        this.months = this.updateMonth(this.months);
+        this.time = (this.years * 12) + this.months;
     }
 
-    calculatePayment() {
-        this.monthlyPayment = (this.loanAmount / this.length).toFixed(2);
+    updateMonth(val): number {
+        const temp: number = this.elValToNum(val);
+        return temp > 12 ? 12 : temp;
     }
 
+    interestRateUpdate(interestPercentage): number {
+        return interestPercentage / 100;
+    }
+
+    calculate(): void {
+        this.table = this.amortization();
+    }
+
+    amortization(): Amortization[] {
+        let month: number = 0;
+        let balance: number = this.loanAmount;
+        const monthlyRate: number = this.interestRateUpdate(this.interestRate) / 12;
+        const terms: number = this.time;
+        this.monthlyPayment = balance * (monthlyRate / (1 - Math.pow(1 + monthlyRate, -terms)));
+
+        let table: Amortization[];
+        table = [];
+        // in-loop interest amount holder
+        let interest: any = 0;
+        // in-loop monthly principal amount holder
+        let monthlyPrincipal = 0;
+        let principal: any = 0;
+
+        for (let count = 0; count < terms; count++) {
+
+            // display the month number in col 1 using the loop count variable
+            month = (count + 1);
+
+            // code for displaying in loop balance
+            balance = parseFloat(balance.toFixed(2));
+
+            // calc the in-loop interest amount
+            interest = parseFloat((balance * monthlyRate).toFixed(2));
+
+            // calc the in-loop monthly principal
+            monthlyPrincipal = this.monthlyPayment - interest;
+            principal = parseFloat(monthlyPrincipal.toFixed(2));
+
+            table.push({
+                month: month,
+                balance: balance,
+                interest: interest,
+                principal: principal
+            });
+
+            // update the balance for each loop iteration
+            balance = balance - monthlyPrincipal;
+        }
+        return table;
+    }
+
+}
+
+
+interface Amortization {
+    month: number;
+    balance: number;
+    interest: number;
+    principal: number;
 }
