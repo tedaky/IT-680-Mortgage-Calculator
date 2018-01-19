03a71c933fc403dcb843d78d01b7b9a1e5058784
Eric Tiedeken <eric.tiedeken@gmail.com>
2018-01-19 09:17:31 -0600

    Added Loan Name to component and view

--- src/app/amortization-calculator/amortization-calculator.component.ts
+++ src/app/amortization-calculator/amortization-calculator.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class AmortizationCalculatorComponent implements OnInit {
 
+    loanName: string;
+
     loanAmount: number;
     time: number;
     interestRate: number;
